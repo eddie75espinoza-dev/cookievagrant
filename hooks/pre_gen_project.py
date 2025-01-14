382baeff4fca32d30c08eec03b7cb5cb4998d736
Eddie Espinoza <eddie.espinoza@prosumia.la>
2025-01-14 19:13:38 -0300

    Agrego prevalidación para descarga de SO

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -5,11 +5,13 @@ UBUNTU_TO_BOX = {
     "24.04": "noble64"
 }
 
+# Verificar si la versión de Ubuntu seleccionada es válida
 if "{{ cookiecutter.ubuntu_version }}" not in UBUNTU_TO_BOX:
     raise ValueError("Versión de Ubuntu no soportada. Use 20.04, 22.04 o 24.04.")
 
 # Asigna automáticamente el nombre de la caja basado en la versión
-cookiecutter_context = "{{ cookiecutter }}".replace(
-    '"box_name": ""', 
-    f'"box_name": "{UBUNTU_TO_BOX["{{ cookiecutter.ubuntu_version }}"]}"'
-)
+box_name = UBUNTU_TO_BOX["{{ cookiecutter.ubuntu_version }}"]
+
+# Sobrescribir directamente el valor de `box_name` en el contexto
+from cookiecutter.main import cookiecutter
+cookiecutter.context['cookiecutter']['box_name'] = box_name
